bd7e22895191142b11cda1a15839b33a351ad481
Leslie <leleu.leslie@outlook.com>
2018-03-07 17:25:54 +0100

    changement valid form + menu css

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,19 @@ $(".form-contact").hide();
 $("#email_span").click(function() {
     $(".form-contact").show();
 });
-//validator form bootstrap
-$('.form-contact').validator();
-//show envoi
+
+//envoi
+function ValidateEmail(email) {
+    var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return expr.test(email);
+};
 $(".btn-envoi").click(function() {
-    $(".form-contact").hide();
+    if (!ValidateEmail($("#email").val())) {
+       //
+        $("#email").css("border-color","red");
+    }
+    else {
+        $(".form-contact").replaceWith("<p>Votre message a été envoyé.</p>");
+    }
 });
+//FIN FORM CONTACT
