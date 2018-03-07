8980562d07d98322990355bfdbb877ad54818e04
Leslie <leleu.leslie@outlook.com>
2018-03-07 14:52:48 +0100

    valid email

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,8 @@ $(".form-contact").hide();
 $("#email_span").click(function() {
     $(".form-contact").show();
 });
+//validator form bootstrap
+$('.form-contact').validator();
 //show envoi
 $(".btn-envoi").click(function() {
     $(".form-contact").hide();
