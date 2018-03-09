40b4472386d70c774ff75f9e467e16178af24e8e
Leslie <leleu.leslie@outlook.com>
2018-03-09 11:58:30 +0100

    deplacement click article
    css + js (event)

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,7 @@ function ValidateEmail(email) {
     var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     return expr.test(email);
 };
+//lorsque l'envoi est validé affiche un message
 $(".btn-envoi").click(function() {
     if (!ValidateEmail($("#email").val())) {
        //
@@ -20,3 +21,30 @@ $(".btn-envoi").click(function() {
     }
 });
 //FIN FORM CONTACT
+
+//ARTICLES
+//au click changement class et move dans div
+var  article ;
+$(".articleMove").click( function() {
+    //si l'article est déjà en principale
+    if ( $(this).hasClass( "articlePrincipale")){
+       $(this).removeClass("articlePrincipale");
+       $(".articleblock").append(this);
+    } else {
+        var bouge = $(".articlePrincipale");
+        $(".articleblock").append(bouge);
+        bouge.removeClass("articlePrincipale");
+        $(this).addClass("articlePrincipale");
+        $('.articleP').append(this);
+    }
+});
+
+/*var articleMove = $('.articleMove').html();
+
+     function articlesMove(){
+        $('.articleMove').html();
+        $('.articlePrincipale').html(articleMove);
+     }*/
+
+
+//FIN ARTICLES
