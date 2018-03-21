2e2b2224f40ec0ac900f62597df07101059cba92
Jeylo <ydervaux@msn.com>
2018-03-21 11:57:57 +0100

    changement classement id

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -19,7 +19,7 @@ class LuckyController extends Controller
     public function affichage(Connection $db) {
         $slider = $db->fetchAll('SELECT * from slider WHERE active=1');
         $articles = $this->getArticles($db);
-        $articles = $db->fetchAll('SELECT * from article ORDER BY date LIMIT 4');
+        $articles = $db->fetchAll('SELECT * from article ORDER BY id DESC LIMIT 4');
         $presentation = $db->fetchAll('SELECT * from presentation');
         //retourne la vue
         return $this->render('accueil.html.twig', [
